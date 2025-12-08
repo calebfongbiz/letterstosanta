4b9e8266be792a179842a86c574ba884b3197209
Caleb Fong <calebfong@Calebs-MacBook-Pro.local>
2025-12-07 23:38:05 -0800

    Fix pixel ID

--- components/MetaPixel.tsx
+++ components/MetaPixel.tsx
@@ -15,7 +15,7 @@ export default function MetaPixel() {
           t.src=v;s=b.getElementsByTagName(e)[0];
           s.parentNode.insertBefore(t,s)}(window, document,'script',
           'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '148272760279422');
+          fbq('init', '1482727602794221');
           fbq('track', 'PageView');
         `}
       </Script>
@@ -24,7 +24,7 @@ export default function MetaPixel() {
           height="1"
           width="1"
           style={{ display: 'none' }}
-          src="https://www.facebook.com/tr?id=148272760279422&ev=PageView&noscript=1"
+          src="https://www.facebook.com/tr?id=1482727602794221&ev=PageView&noscript=1"
           alt=""
         />
       </noscript>
